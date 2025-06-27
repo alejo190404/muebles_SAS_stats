c6c584cbe25c47ff78cc11d3376dd11c12301bf4
alejo190404 <alejo190404@gmail.com>
2025-06-27 15:19:51 -0500

    Modelo Stat. Clase creada y Atributos definidos

---
+++ scaffold-muebles-sas-stats/domain/model/src/main/java/co/com/bancolombia/model/stat/Stat.java
@@ -0,0 +1,22 @@
+package co.com.bancolombia.model.stat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Stat {
+    private Integer totalContactoClientes;
+    private Integer motivoReclamo;
+    private Integer motivoGarantia;
+    private Integer motivoDuda;
+    private Integer motivoCompra;
+    private Integer motivoFelicitaciones;
+    private Integer motivoCambio;
+    private String hash;
+}
