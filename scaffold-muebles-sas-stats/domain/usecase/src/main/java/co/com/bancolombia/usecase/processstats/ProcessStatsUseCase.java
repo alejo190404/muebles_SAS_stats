09642e3be45823d1664f540841c65818738f5213
alejo190404 <alejo190404@gmail.com>
2025-06-29 13:10:50 -0500

    Driven adapter created

--- scaffold-muebles-sas-stats/domain/usecase/src/main/java/co/com/bancolombia/usecase/processstats/ProcessStatsUseCase.java
+++ scaffold-muebles-sas-stats/domain/usecase/src/main/java/co/com/bancolombia/usecase/processstats/ProcessStatsUseCase.java
@@ -2,14 +2,25 @@ package co.com.bancolombia.usecase.processstats;
 
 import co.com.bancolombia.model.stat.Stat;
 import lombok.RequiredArgsConstructor;
+
 @RequiredArgsConstructor
 public class ProcessStatsUseCase {
-    public String exposeEndpointTest(){
+
+    public String exposeEndpointTest() {
         return "Endpdoint exposed appropiately";
     }
 
-    public boolean processStats(Stat stat){
-        //Logic to process the stats accordingly to requirements
-        return true; //Hardcoded
+    public boolean processStats(Stat stat) {
+        // if hash is correct, add it to DB and publish it to rabbitMQ
+        if (true){
+            // DynamoDB. Add with primary key as timestamp
+            
+
+            // RabbitMQ. Add it to queue "event.stats.validated"
+
+        }
+        
+        // if hash is not correct, return 400 and error message
+        return false;
     }
 }
